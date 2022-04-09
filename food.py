c1ec17c8b4b2bed591b2758521e2973e4b3d5edd
mbedniy777 <mbedniy777@gmail.com>
2022-04-09 23:30:48 +0300

    Create class Food

--- food.py
+++ food.py
@@ -1,5 +1,17 @@
+from random import randrange
+
 import pygame as pg
+from settings import PIXEL, WIGHT, HEIGHT
 
 
-class Food:
-    pass
+class Food(pg.Rect):
+    def __init__(self, color):
+        self.color = color
+        super().__init__(0, 0, PIXEL, PIXEL)
+
+    def draw(self, surface):
+        pg.draw.rect(surface, self.color, self)
+
+    def set_random_cords(self):
+        self.left = randrange(0, WIGHT, PIXEL)
+        self.top = randrange(0, HEIGHT, PIXEL)
