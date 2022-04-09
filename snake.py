c3e1019a19d6e9d456a70aaf1fc22baf591f51a8
mbedniy777 <mbedniy777@gmail.com>
2022-04-09 18:40:39 +0300

    Added snake movement functionality

--- snake.py
+++ snake.py
@@ -1,2 +1,36 @@
+import pygame as pg
+from settings import PIXEL, WIGHT, HEIGHT
+
+
 class Snake:
-    pass
+    RIGHT_VECTOR = 1
+    LEFT_VECTOR = 2
+    UP_VECTOR = 3
+    DOWN_VECTOR = 4
+
+    def __init__(self):
+        self.body = [pg.Rect((WIGHT / 2, HEIGHT / 2), PIXEL, PIXEL)]
+        self.vector = self.RIGHT_VECTOR
+
+    def move(self):
+        snake_head = self.body[0].copy()
+        self.body.pop()
+        self.body.insert(0, snake_head)
+        if self.vector == self.RIGHT_VECTOR:
+            snake_head.left += PIXEL
+        elif self.vector == self.LEFT_VECTOR:
+            snake_head.left -= PIXEL
+        elif self.vector == self.UP_VECTOR:
+            snake_head.top -= PIXEL
+        elif self.vector == self.DOWN_VECTOR:
+            snake_head.top += PIXEL
+
+    def change_vector(self):
+        pass
+
+    def increase(self):
+        pass
+
+    def draw(self):
+        pass
+
